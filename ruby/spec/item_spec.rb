8efe091984faa9db548894a01661b1e0589f91d2
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-03-06 12:23:52 +0000

    Tests: All tests passing for normal items

--- ruby/spec/item_spec.rb
+++ ruby/spec/item_spec.rb
@@ -17,12 +17,32 @@ describe Item do
 
     describe "Update Normal Items" do
 
-      shop = GildedRose.new(item)
-
       it "checks that the sell by date updates as expected" do
+        shop = GildedRose.new(item)
         shop.update_quality
         expect(item[0].sell_in).to eq(9)
+      end
 
+      it "checks that items degarde twice as fast after sell by date" do
+        item = [Item.new("My goodies", 0, 10)]
+        shop = GildedRose.new(item)
+        shop.update_quality
+        expect(item[0].quality).to eq(8)
+      end
+
+      it "checks that quality is never negative" do
+        item = [Item.new("My goodies", 10, 0)]
+        shop = GildedRose.new(item)
+        shop.update_quality
+        expect(item[0].quality).to eq(0)
+      end
+
+      it "checks that items can be update correctly multiple times" do
+        item = [Item.new("My goodies", 1, 10)]
+        shop = GildedRose.new(item)
+        shop.update_quality
+        shop.update_quality
+        expect(item[0].quality).to eq(7)
       end
     end
 end
