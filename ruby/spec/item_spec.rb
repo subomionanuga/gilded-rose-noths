4d1f6943a18298fc15d203c2cb3e5eee06e511d7
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-03-06 13:19:18 +0000

    Tests: Full coverage. Test for sulfuras error failing as no code written yet

--- ruby/spec/item_spec.rb
+++ ruby/spec/item_spec.rb
@@ -100,4 +100,21 @@ describe Item do
       expect(item[0].quality).to eq(0)
     end
   end
+
+  describe "Legendary Item" do
+
+    it "checks that the quality of Sulfuras cannot be anything but 80" do
+      item = [Item.new("Sulfuras, Hand of Ragnaros", 0, 70)]
+      shop = GildedRose.new(item)
+      shop.update_quality
+      expect { item[0].quality }.to raise_error("Sulfuras can only be 80")
+    end
+
+    it "checks that Sulfuras quality cannot change" do
+      item = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+      shop = GildedRose.new(item)
+      shop.update_quality
+      expect(item[0].quality).to eq(80)
+    end
+  end
 end
