4bf0c7aa8d5c2fbdc1f54c18e3b294a9fdff7242
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-03-07 12:35:05 +0000

    Feature: conjured items can degrade properly

--- ruby/lib/conjured.rb
+++ ruby/lib/conjured.rb
@@ -1,4 +1,21 @@
 require_relative "item"
 
 class Conjured < Item
+
+  def reduce_sell_in
+    new_quality = @quality + self.quality_change
+    if new_quality < 0
+      @quality = 0
+    elsif new_quality > 50
+      @quality = 50
+    else
+      @quality = new_quality
+    end
+    @sell_in -= 1
+  end
+
+  def quality_change
+     @sell_in < 0 ? -4 : -2
+  end
+
 end
--- ruby/lib/normal_item.rb
+++ ruby/lib/normal_item.rb
@@ -2,17 +2,17 @@ require_relative "item"
 
 class NormalItem < Item
 
-def reduce_sell_in
-  new_quality = @quality + self.quality_change
-  if new_quality < 0
-    @quality = 0
-  elsif new_quality > 50
-    @quality = 50
-  else
-    @quality = new_quality
+  def reduce_sell_in
+    new_quality = @quality + self.quality_change
+    if new_quality < 0
+      @quality = 0
+    elsif new_quality > 50
+      @quality = 50
+    else
+      @quality = new_quality
+    end
+    @sell_in -= 1
   end
-  @sell_in -= 1
-end
 
   def quality_change
      @sell_in < 0 ? -2 : -1
