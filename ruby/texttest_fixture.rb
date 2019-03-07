5ed7e7a0d818ce1b3115fe9369af927ff1055caa
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-03-07 10:36:45 +0000

    Test: texttest file updated to check that logic is working as expected

--- ruby/texttest_fixture.rb
+++ ruby/texttest_fixture.rb
@@ -3,19 +3,21 @@
 require "./lib/gilded_rose"
 require "./lib/item"
 require "./lib/aged_brie"
+require "./lib/sulfuras"
+require "./lib/backstage_passes"
 
 puts "OMGHAI!"
 items = [
-  Item.new(name = "+5 Dexterity Vest", sell_in = 10, quality = 20),
+  NormalItem.new(name = "+5 Dexterity Vest", sell_in = 10, quality = 20),
   AgedBrie.new(sell_in = 2, quality = 0),
-  Item.new(name = "Elixir of the Mongoose", sell_in = 5, quality = 7),
-  Item.new(name = "Sulfuras, Hand of Ragnaros", sell_in = 0, quality = 80),
-  Item.new(name = "Sulfuras, Hand of Ragnaros", sell_in = -1, quality = 80),
-  Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 15, quality = 20),
-  Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 10, quality = 49),
-  Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 5, quality = 49),
+  NormalItem.new(name = "Elixir of the Mongoose", sell_in = 5, quality = 7),
+  Sulfuras.new(sell_in = 0, quality = 80),
+  Sulfuras.new(sell_in = -1, quality = 80),
+  BackstagePasses.new(sell_in = 15, quality = 20),
+  BackstagePasses.new(sell_in = 10, quality = 49),
+  BackstagePasses.new(sell_in = 5, quality = 49),
   # This Conjured item does not work properly yet
-  Item.new(name = "Conjured Mana Cake", sell_in = 3, quality = 6), # <-- :O
+  NormalItem.new(name = "Conjured Mana Cake", sell_in = 3, quality = 6), # <-- :O
 ]
 
 days = 2
